375e758499d52349417285e055764cc457e04bf9
Rondo Lehner <rondo.lehner@gmail.com>
2022-08-21 23:50:28 +0200

    formatting

--- problems/set_matrix_zeroes/solution.py
+++ problems/set_matrix_zeroes/solution.py
@@ -4,10 +4,12 @@ from typing import Iterable
 class Solution:
     # Idea:
     # 1. Extract and store the columns that need to be set to 0
-    # 2. Set any rows that contain a 0 to zero
+    # 2. Set any rows that contain a 0 to 0
     # 3. Based on 1 set the cells corresponding to 'zero' columns to 0
 
-    # -> Solution uses space with O(n) and time with O(nm)
+    # Complexities:
+    # * Space O(n)
+    # * Time  O(nm)
     
     def setZeroes(matrix: Iterable[Iterable[int]]) -> None:
         zero_cols = set()
@@ -23,8 +25,6 @@ class Solution:
             for i in range(len(row)):
                 row[i] = 0
 
-            
-        
         for row in matrix:
             for i in zero_cols:
                 row[i] = 0
