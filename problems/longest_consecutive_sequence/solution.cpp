b8082f3dcaab83bd5b26eb6a5027aa645e586371
Rondo Lehner <rondo.lehner@gmail.com>
2022-08-23 22:01:27 +0200

    added comments outlining the idea to the approach

--- problems/longest_consecutive_sequence/solution.cpp
+++ problems/longest_consecutive_sequence/solution.cpp
@@ -1,4 +1,10 @@
 class Solution {
+
+    // Idea (sub-optimal as the time complexity is larger than O(n) as it depends on the range of possible values) 
+    // 1. Initialise an array of length equal to the range between the min and max value in the given array (assuming this value is provided)
+    // 2. Iterate over 'nums' and store 1 at the index corresponding with the current number
+    // 3. Iterate over the "hash map" array and track the starting index and length of the longest consecutive sequence of 1s
+
 public:
     int longestConsecutive(vector<int>& nums) {
         
