526c968fe74ef9f458cbb06e0f8ad27cde52aec2
Rondo Lehner <rondo.lehner@gmail.com>
2022-08-23 21:32:42 +0200

    added solution for the 'spiral matrix' problem

--- problems/spiral_matrix/solution.py
+++ problems/spiral_matrix/solution.py
@@ -1,40 +1,66 @@
 from typing import List
 
+# Idea:
+# 1. Start in a direction until the boundary of the matrix is reached
+# 2. Change the direction by 90 degrees
+# 3. Eliminate the column/row which was traversed from the traversable matrix
+# 4. Repeat Step 1 until the result contains n*m entries from the original matrix
+
 class Solution:
     def spiralOrder(matrix: List[List[int]]) -> List[int]:
         m = len(matrix)
         n = len(matrix[0])
 
-        r = 0 # track current row
-        c = 0 # track current column
+        b_t = 0                     # top
+        b_b = m - 1                 # bottom
+        b_l = 0                     # left
+        b_r = n - 1                 # right
+
+        cursor = {
+            'row': 0,
+            'col': 0
+        }
         
         directions = [1, 0, -1, 0] # defines sequence of direction switching
 
-        dx = 0 # index of current direction in terms of moving left to right
-        dy = 3 # index of current direction in terms of moving top to bottom
+        direction_indices = {
+            'col': 0,   # index referring to direction which horizontally traverses the matrix
+            'row': 3    # index referring to direction which vertically traverses the matrix
+        }
 
         res = []
 
         for i in range(m * n):
-            res.append(matrix[r][c])
-            r += directions[dy]
-            c += directions[dx]
+            res.append(matrix[cursor['row']][cursor['col']])
 
-            if m == 0 and n == 0: break
+            if directions[direction_indices['col']] == 1 and cursor['col'] == b_r:
+                b_t += 1
+                Solution.changeDirection(direction_indices)
 
-            if n != 0 and (c + 1) % n == 0:
-                n -= 1
-                dx = (dx + 1) % len(directions)
-                dy = (dy + 1) % len(directions)
+            if directions[direction_indices['col']] == -1 and cursor['col'] == b_l:
+                b_b -= 1
+                Solution.changeDirection(direction_indices)
+
+            if directions[direction_indices['row']] == 1 and cursor['row'] == b_b:
+                b_r -= 1
+                Solution.changeDirection(direction_indices)
             
-            if m != 0 and (r + 1) % m == 0:
-                m -= 1
-                dy = (dy + 1) % len(directions)
-                dx = (dx + 1) % len(directions)    
-            
-            print(r, c)
+            if directions[direction_indices['row']] == -1 and cursor['row'] == b_t:
+                b_l += 1
+                Solution.changeDirection(direction_indices)
+
+            cursor['row'] += directions[direction_indices['row']]
+            cursor['col'] += directions[direction_indices['col']]
 
         return res
+    
+    @staticmethod
+    def changeDirection(direction_indices):
+        direction_indices['col'] = (direction_indices['col'] + 1) % 4
+        direction_indices['row'] = (direction_indices['row'] + 1) % 4
+
+        return direction_indices
+
 
 matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 
