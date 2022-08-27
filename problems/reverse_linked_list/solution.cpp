76dfe4452992b3027d6aa1d10d4fd6e83a026d96
Rondo Lehner <rondo.lehner@gmail.com>
2022-08-27 21:41:07 +0200

    added solution for 'reverse linked list'

--- problems/reverse_linked_list/solution.cpp
+++ problems/reverse_linked_list/solution.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 struct ListNode {
     int val;
     ListNode *next;
@@ -7,8 +9,44 @@ struct ListNode {
 };
 
 class Solution {
+    // Implemented the iterative solution
+    
 public:
-    ListNode* reverseList(ListNode* head) {
-        
+    static ListNode* reverseList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) return head;
+        ListNode *prev = nullptr;
+        while(head->next != nullptr){
+            ListNode* temp = head->next;
+            head->next = prev;
+            prev = head;
+            head = temp;
+        }
+        head->next = prev;
+        return head;
+    }
+
+    static void printList(ListNode* head) {
+        if(head == nullptr) return;
+        while(head->next != nullptr){
+            std::cout << head->val << std::endl;
+            head = head->next;
+        }
+        std::cout << head->val << std::endl;
+        return;
     }
 };
+
+int main(){
+    ListNode* node1 = new ListNode(5);
+    ListNode* node2 = new ListNode(4, node1);
+    ListNode* node3 = new ListNode(3, node2);
+    ListNode* node4 = new ListNode(2, node3);
+    ListNode* head = new ListNode(1, node4);
+
+    Solution::printList(head);
+    std::cout << "reversed list: " << std::endl;
+    head = Solution::reverseList(head);
+    Solution::printList(head);
+
+    return -1;
+}
