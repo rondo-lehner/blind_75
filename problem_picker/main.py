58a8c7f792e87b3ba8b999aa60c8e10f21c963e9
Rondo Lehner <rondo.lehner@gmail.com>
2022-08-28 19:45:09 +0200

    modified problem_picker to update list of problems_solved

--- problem_picker/main.py
+++ problem_picker/main.py
@@ -1,9 +1,13 @@
 import random
 from problems_all import *
-from problems_solved import *
+# from problems_solved import *
 
-problems_to_solve = [p for p in problems_all if p not in problems_solved]
+with open("problems_solved.txt", "a+") as f:
+    problems_solved = f.readlines()
+    problems_to_solve = [p for p in problems_all if p not in problems_solved]
+    problem_to_solve = random.choice(problems_to_solve)
+    f.write("\n" + problem_to_solve)
 
-print(random.choice(problems_to_solve))
+print(problem_to_solve)
 
 
--- problem_picker/problems_solved.py
+++
@@ -1,7 +0,0 @@
-problems_solved = [
-    "Set Matrix Zeroes",
-    "Reverse a Linked List",
-    "Spiral Matrix",
-    "Longest Consecutive Sequence",
-    "Longest Repeating Character Replacement"
-]
